e70e085f230f63f50ece60d7f5db7343b1bd6966
DiamondAngel1 <tararukolga78@gmail.com>
2024-06-30 20:41:37 +0300

    Create project

---
+++ Project1/FileName.cpp
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS  
+#include <iostream>
+#include <Windows.h>
+#include <string.h>
+using namespace std;
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    
+}
