c068bfe9eee45a01e692ceca71cb63656891b006
DiamondAngel1 <tararukolga78@gmail.com>
2024-06-30 20:43:22 +0300

    Function UserInput

--- Project1/FileName.cpp
+++ Project1/FileName.cpp
@@ -3,8 +3,22 @@
 #include <Windows.h>
 #include <string.h>
 using namespace std;
+
+void UserInput(char* user, char* wordSearch, int maxUserLength, int maxWordLength) {
+    cout << "¬вед≥ть р€док: ";
+    cin.getline(user, maxUserLength);
+    cout << "¬вед≥ть слово дл€ п≥драхунку: ";
+    cin.getline(wordSearch, maxWordLength);
+}
+
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    
+    const int max = 500;
+    char user[max];
+    char wordSearch[50];
+    const char* pattern = " .,!?;:";
+    UserInput(user, wordSearch, max, 50);
+
+    return 0;
 }
